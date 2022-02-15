21681e0d067150df6c76ed52cb167ab9e68fdd92
Rayanne Barros <rayanneb126@gmail.com>
2022-02-14 22:46:16 -0300

    Criando botão

---
+++ script.js
@@ -0,0 +1,64 @@
+const controles = document.querySelector('#controles');
+const cssText = document.querySelector('.css');
+const btn = document.querySelector('.btn');
+
+controles.addEventListener('change', handleChange);
+
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + 'px';
+  },
+  width(value) {
+    this.element.style.width = value + 'px';
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + 'px';
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + 'rem';
+  },
+
+}
+
+function handleChange(e) {
+  const name = e.target.name;
+  const value = e.target.value;
+
+  handleStyle[name](value);
+  salveValues(name, value);
+  showCss();
+}
+
+function salveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach((propertie) => {
+    handleStyle[propertie](localStorage[propertie]);
+    controles.elements[propertie].value = localStorage[propertie];
+  });
+  showCss();
+}
+setValues()
+
+function showCss() {
+  cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
+}
